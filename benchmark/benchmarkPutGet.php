cdd5d4a79fa96913097a059f74b2114f3be0d126
sachin.vm <smurthy@aerospike.com>
2024-03-13 15:07:59 -0700

    made changes to benchmark

--- benchmark/benchmarkPutGet.php
+++ benchmark/benchmarkPutGet.php
@@ -20,91 +20,144 @@ class AerospikeBenchmark {
 
     /**
      * @Iterations(5)
-     * @Revs(1)
+     * @BeforeMethods("makeDataForGetBenchString1")
      */
-    public function benchGet(): void {
-        $this->benchGetString1();
-        $this->benchGetString10();
-        $this->benchGetString100();
-        $this->benchGetString1000();
-        $this->benchGetString10000();
-        $this->benchGetInteger32();
-        $this->benchGetInteger64();
-    }
-
     public function benchGetString1(): void {
         $set = "Benchmark_Get_String1";
+        $this->doGet($set);
+    }
+
+    public function makeDataForGetBenchString1() {
+        $set = "Benchmark_Get_String1";
         $value = str_repeat("s", 1);
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public function benchGetString10(): void {
-        $set = "Benchmark_Get_String10";
-        $value = str_repeat("s", 10);
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public function benchGetString100(): void {
-        $set = "Benchmark_Get_String100";
-        $value = str_repeat("s", 100);
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public function benchGetString1000(): void {
-        $set = "Benchmark_Get_String1000";
-        $value = str_repeat("s", 1000);
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public function benchGetString10000(): void {
-        $set = "Benchmark_Get_String10000";
-        $value = str_repeat("s", 10000);
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public function benchGetInteger32(): void {
-        $set = "Benchmark_Get_Integer32";
-        $value = 2147483647; // Maximum value for a 32-bit signed integer
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public function benchGetInteger64(): void {
-        $set = "Benchmark_Get_Integer64";
-        $value = 9223372036854775807; // Maximum value for a 64-bit signed integer
-        $this->makeDataForGetBench($set, "b", $value);
-        gc_collect_cycles();
-        $this->doGet($set);
-    }
-
-    public static function makeDataForGetBench($set, $binName, $binValue) {
         $wp = new WritePolicy();
         for ($k = 0; $k < 1000; $k++) {
             $key = new Key(self::$namespace, $set, $k);
-            self::$client->put($wp, $key, [new Bin($binName, $binValue)]);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
         }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @Iterations(5)
+     * @BeforeMethods("makeDataForGetBenchString10")
+     */
+    public function benchGetString10(): void {
+        $set = "Benchmark_Get_String10";
+        $this->doGet($set);
+    }
+    public function makeDataForGetBenchString10() {
+        $set = "Benchmark_Get_String1";
+        $value = str_repeat("s", 10);
+        $wp = new WritePolicy();
+        for ($k = 0; $k < 1000; $k++) {
+            $key = new Key(self::$namespace, $set, $k);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
+        }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @Iterations(5)
+     * @BeforeMethods("makeDataForGetBenchString100")
+     */
+    public function benchGetString100(): void {
+        $set = "Benchmark_Get_String100";
+        $this->doGet($set);
+    }
+    public function makeDataForGetBenchString100() {
+        $set = "Benchmark_Get_String1";
+        $value = str_repeat("s", 100);
+        $wp = new WritePolicy();
+        for ($k = 0; $k < 1000; $k++) {
+            $key = new Key(self::$namespace, $set, $k);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
+        }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @Iterations(5)
+     * @BeforeMethods("makeDataForGetBenchString1000")
+     */
+    public function benchGetString1000(): void {
+        $set = "Benchmark_Get_String1000";
+        $this->doGet($set);
+    }
+    public function makeDataForGetBenchString1000() {
+        $set = "Benchmark_Get_String1";
+        $value = str_repeat("s", 1000);
+        $wp = new WritePolicy();
+        for ($k = 0; $k < 1000; $k++) {
+            $key = new Key(self::$namespace, $set, $k);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
+        }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @Iterations(5)
+     * @BeforeMethods("makeDataForGetBenchString10000")
+     */
+    public function benchGetString10000(): void {
+        $set = "Benchmark_Get_String10000";
+        $this->doGet($set);
+    }
+    public function makeDataForGetBenchString10000() {
+        $set = "Benchmark_Get_String1";
+        $value = str_repeat("s", 10000);
+        $wp = new WritePolicy();
+        for ($k = 0; $k < 1000; $k++) {
+            $key = new Key(self::$namespace, $set, $k);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
+        }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @Iterations(5)
+     * @BeforeMethods("makeDataForGetBenchInt32")
+     */
+    public function benchGetInteger32(): void {
+        $set = "Benchmark_Get_Integer32";
+        $this->doGet($set);
+    }
+    public function makeDataForGetBenchInt32() {
+        $set = "Benchmark_Get_Integer32";
+        $value = 2147483647; 
+        $wp = new WritePolicy();
+        for ($k = 0; $k < 1000; $k++) {
+            $key = new Key(self::$namespace, $set, $k);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
+        }
+        gc_collect_cycles();
+    }
+
+    /**
+     * @Iterations(5)
+     * @BeforeMethods("makeDataForGetBenchInt64")
+     */
+    public function benchGetInteger64(): void {
+        $set = "Benchmark_Get_Integer64";
+        $this->doGet($set);
+    }
+    public function makeDataForGetBenchInt64() {
+        $set = "Benchmark_Get_Integer64";
+        $value = 9223372036854775807; 
+        $wp = new WritePolicy();
+        for ($k = 0; $k < 1000; $k++) {
+            $key = new Key(self::$namespace, $set, $k);
+            self::$client->put($wp, $key, [new Bin("b", $value)]);
+        }
+        gc_collect_cycles();
     }
 
     public function doGet($set) {
         $rp = new ReadPolicy();
-        for ($i = 0; $i < 100; $i++) {
-            $key = new Key(self::$namespace, $set, 0);
-            $record = self::$client->get($rp, $key);
-        }
+        $key = new Key(self::$namespace, $set, rand(0,100));
+        $record = self::$client->get($rp, $key);
     }
 }
 
 $bench = new AerospikeBenchmark();
 $bench->setUpBeforeClass();
-$bench->benchGet();
