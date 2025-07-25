b7758853970a3e93323bf058b97735bcd3fe07ab
Asparuh Nestorov <a.nestorov@adcash.com>
2025-07-25 13:45:52 +0300

    Add send key setter and getter to BatchWritePolicy

--- php_stubs/libaerospike-php-stubsv1.0.0.php
+++ php_stubs/libaerospike-php-stubsv1.0.0.php
@@ -3777,6 +3777,8 @@ namespace Aerospike {
 
         public $record_exists_action;
 
+        public $send_key;
+
         public function __construct() {}
 
         /**
@@ -3857,6 +3859,16 @@ namespace Aerospike {
         public function getDurableDelete(): bool {}
 
         public function setDurableDelete(bool $durable_delete) {}
+
+        /**
+         * SendKey determines to whether send user defined key in addition to hash digest on both reads and writes.
+         * If the key is sent on a write, the key will be stored with the record on
+         * the server.
+         * The default is to not send the user defined key.
+         */
+        public function getSendKey(): bool {}
+
+        public function setSendKey(bool $send_key) {}
     }
 
     /**
